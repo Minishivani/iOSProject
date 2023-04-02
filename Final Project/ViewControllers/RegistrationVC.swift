ef400242fdb711d978a5541a650dc12bef3dc4dc
Sai Karthik Malyala <108293822+MalyalaSaiKarthik@users.noreply.github.com>
2023-04-02 16:21:42 -0500

    Updated registration page for user registration.

---
+++ Final Project/ViewControllers/RegistrationVC.swift
@@ -0,0 +1,135 @@
+//
+//  Registration.swift
+//  Final Project
+//
+//  Created by Sai Karthik Malyala on 4/2/23.
+//
+
+import UIKit
+import Firebase
+
+class RegistrationVC: UIViewController {
+    
+    
+    @IBOutlet weak var firstNameTF: UITextField!
+    
+    @IBOutlet weak var middleNameTF: UITextField!
+    
+    @IBOutlet weak var lastNameTF: UITextField!
+    
+    @IBOutlet weak var emailTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBOutlet weak var confirmPasswordTF: UITextField!
+    
+    @IBOutlet weak var signUpBTN: UIButton!
+    
+    
+    var email: String = ""
+    var password: String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if email != "" {
+            emailTF.text = email
+            if password != "" {
+                passwordTF.text = password
+            }
+        }
+    }
+    
+    @IBAction func signUp(_ sender: UIButton) {
+        guard let firstName = firstNameTF.text, !firstName.isEmpty else {
+            showAlert(title: "", message: "Please enter First name.")
+            return
+        }
+        guard let lastName = lastNameTF.text, !lastName.isEmpty else {
+            showAlert(title: "", message: "Please enter Last name.")
+            return
+        }
+        guard let email = emailTF.text, !email.isEmpty else {
+            showAlert(title: "", message: "Please enter Email-ID.")
+            return
+        }
+        guard NSPredicate(format:"SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}").evaluate(with: email) else {
+            showAlert(title: "", message: "Please enter validate Email-ID.")
+            return
+        }
+        guard let password = passwordTF.text, !password.isEmpty else {
+            showAlert(title: "", message: "Please enter password.")
+            return
+        }
+        guard let confirmPassword = confirmPasswordTF.text, !confirmPassword.isEmpty else {
+            showAlert(title: "", message: "Please enter confirm password.")
+            return
+        }
+        guard password == confirmPasswordTF.text else {
+            showAlert(title: "", message: "Password and confirm password are not matching.")
+            return
+        }
+        guard password.count > 5 else {
+            showAlert(title: "", message: "Password length must be atleast 6 characters.")
+            return
+        }
+        guard let middleName = middleNameTF.text else {return}
+        
+        let auth = FirebaseAuth.Auth.auth()
+        
+        auth.fetchSignInMethods(forEmail: email) { (providers, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let providers = providers {
+                if providers.count > 0 {
+                    let alert = UIAlertController(title: "", message: "User already exists", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {(action) in
+                        self.navigationController?.popToRootViewController(animated: true)
+                    }))
+                    self.present(alert, animated: true, completion: nil)
+                    return
+                }
+            }
+        }
+        auth.createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print(error)
+                return
+            } else if let authResult = authResult {
+                let db = Firestore.firestore()
+                let userRef = db.collection("users").document(authResult.user.uid)
+                userRef.setData(["firstname": firstName])
+                if !middleName.isEmpty {
+                    userRef.setData(["middlename": middleName])
+                }
+                userRef.setData(["lastname": lastName])
+            }
+            let alert = UIAlertController(title: "", message: "User registered successfully.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {(action) in
+                self.navigationController?.popToRootViewController(animated: true)
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func toLoginPage(_ sender: UIButton) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Final Project/ViewControllers/RootVC.swift
+++ Final Project/ViewControllers/RootVC.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class RootVC: UIViewController {
 
+    @IBOutlet weak var usernameTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBOutlet weak var loginBTN: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func login(_ sender: UIButton) {
+    }
+    
+    @IBAction func userRegistration(_ sender: UIButton) {
+        performSegue(withIdentifier: "registration", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let segueName = segue.identifier {
+            switch segueName {
+            case "registration":
+                if let destinationVC = segue.destination as? RegistrationVC {
+                    if let email = usernameTF.text {
+                        destinationVC.email = email
+                        if let password = passwordTF.text {
+                            destinationVC.password = password
+                        } else {
+                            destinationVC.password = ""
+                        }
+                    }
+                }
+            default:
+                break
+            }
+        }
+    }
+    
 }
 
